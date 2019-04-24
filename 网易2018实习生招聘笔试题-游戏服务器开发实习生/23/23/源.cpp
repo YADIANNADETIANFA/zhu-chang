f8dc086f328b600b7bce74ccb6d86c27112ed07f
YADIANNADETIANFA <871227769@qq.com>
2019-04-24 13:40:32 +0800

    upload codes

---
+++ 网易2018实习生招聘笔试题-游戏服务器开发实习生/23/23/源.cpp
@@ -0,0 +1,71 @@
+//递归
+#include<cstdio>
+#include<algorithm>
+#include<vector>
+#include<iostream>
+using namespace std;
+
+const int MAX = 35;
+struct Food
+{
+	long long weight = 0;
+	//bool InBag = false;
+}food[MAX];//必须常量值
+
+bool compare(Food food1, Food food2)
+{
+	return food1.weight < food2.weight;
+}
+
+long long Num_of_save(vector<Food> food_weight, long long num_of_food, long long remain_bag_contain,int num_of_food_needIn,int select_band)//主要递归函数
+{
+	if (num_of_food_needIn == 0 && remain_bag_contain >= 0)//一共要放入3袋，先放入1袋，再放入1袋，再放入1袋，看放完后是否符合要求，
+		return 1;                                           //如果该方案满足，结果就加1
+
+	int count = 0;//举例，如果共3袋已取2袋，那最后一袋还有很多取法，用count一一列出
+	for (int i = select_band; i < num_of_food; ++i)//1、2+3和1+2、3这两种取三袋方式相同，属于重复，为避免，我们只取目前该袋后面袋的零食，只向右不会左
+	{
+		if (food_weight[i].weight > remain_bag_contain)//加上后，AC从80到100，复杂度也不超时了
+			break;
+		count += Num_of_save(food_weight, num_of_food, remain_bag_contain - food_weight[i].weight, num_of_food_needIn - 1, i + 1);
+	}
+	return count;
+}
+int main()
+{
+	long long num_of_food, bag_contain;
+	while (cin >> num_of_food >> bag_contain)
+	{
+		vector<Food> food_weight;
+		for (int i = 0; i < num_of_food; ++i)
+		{
+			cin >> food[i].weight;
+			food_weight.push_back(food[i]);
+		}
+		sort(food_weight.begin(), food_weight.begin() + num_of_food, compare);
+
+		long long ans = 1;//初始啥也不装
+		int get_max = 0;//最多装几包零食
+		long long contain_current = 0;
+		while (contain_current <= bag_contain && get_max < num_of_food)
+		{
+			contain_current += food_weight[get_max].weight;
+			++get_max;
+		}
+		if (contain_current > bag_contain)
+			--get_max;
+
+		if (get_max == num_of_food)//加上后AC从60到80，复杂度也不超时了
+			ans = pow(2, get_max);
+		else
+		{
+			for (int i = 1; i <= get_max; ++i)
+			{
+				ans += Num_of_save(food_weight, num_of_food, bag_contain, i, 0);
+
+			}
+		}
+		cout << ans << endl;
+	}
+	return 0;
+}
